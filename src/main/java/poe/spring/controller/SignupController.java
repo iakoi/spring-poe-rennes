89e292d0d21e1aec50e11af781285a944b72bea9
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-22 11:24:23 +0100

    validation du formulaire inscription

--- src/main/java/poe/spring/controller/SignupController.java
+++ src/main/java/poe/spring/controller/SignupController.java
@@ -1,31 +1,50 @@
 package poe.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import poe.spring.form.LoginForm;
+import poe.spring.service.UserManagerService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
 
+    @Autowired
+    UserManagerService userManagerService;
+
     @GetMapping
-    public String index() {
-        return "signup";
+    public String index(LoginForm form) {
+        return "signup"; // affiche signup.html
     }
 
     @PostMapping
-    public String save(String userName, RedirectAttributes attr) {
-        System.out.println("name " + userName);
-        // TODO faire la sauvegarde de l'utilisateur
-        attr.addAttribute("userName", userName);
+    public String save(@Valid LoginForm form,
+                       BindingResult bindingResult,
+                       RedirectAttributes attr) {
+
+        System.out.println("login " + form.getLogin());
+        System.out.println("password " + form.getPassword());
+
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        userManagerService.signup(form.getLogin(), form.getPassword());
+        attr.addAttribute("userName", form.getLogin());
         return "redirect:/signup/success";
     }
 
     @GetMapping("/success")
-    public String success(String userName, Model model) {
+    public String success(@RequestParam("userName") String userName, Model model) {
         model.addAttribute("name", userName);
         return "success"; // success.html
     }
---
+++ src/main/java/poe/spring/form/LoginForm.java
@@ -0,0 +1,28 @@
+package poe.spring.form;
+
+import javax.validation.constraints.Size;
+
+public class LoginForm {
+
+    @Size(min = 2)
+    private String login;
+
+    @Size(min = 1)
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
